d2ec1421f312dee64ed691a8b202fb364aeac61f
李晨 <lichen@lichendeMacBook-Pro.local>
2024-08-06 17:47:47 +0800

    feat:新增前后端容器话打包

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate rocket;
-
-use rocket::serde::Serialize;
 use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+use rocket_cors::{AllowedOrigins, CorsOptions };
 
 #[derive(Debug, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -23,5 +23,21 @@ fn test() -> Json<MyResponse> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/data", routes![test])
+    // 定义允许的跨域源
+    let allowed_origins = AllowedOrigins::some_exact(&[
+        "http://127.0.0.1",
+        "http://112.74.46.63",
+        "http://127.0.0.1:5173"
+    ]);
+
+    // 配置 CORS 选项
+    let cors = CorsOptions {
+        allowed_origins,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap(); // `unwrap` 是为了简化示例，实际应用中应处理可能的错误
+    rocket::build()
+        .attach(cors)
+        .mount("/data", routes![test])
 }
